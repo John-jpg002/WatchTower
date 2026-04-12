dae82ae03a2be574c62ebd86f6e3e350885b52d6
qkeequicoy <qkeequicoy@tip.edu.ph>
2026-04-12 13:35:56 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,13 +4,11 @@ import 'screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Supabase.initialize(
     url: 'https://afjmndbunsqpaphikjqg.supabase.co',
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmam1uZGJ1bnNxcGFwaGlranFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzMDg0MjIsImV4cCI6MjA5MDg4NDQyMn0.yeVLn_Xm9p7zNm2zq4CsFESFyRbZxfXLEuopRVz_9Mk',
   );
-
   runApp(const WatchtowerApp());
 }
 
